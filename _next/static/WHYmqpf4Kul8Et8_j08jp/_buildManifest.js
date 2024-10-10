d5ce1e81eb6ab8cd3a5e76c292b54b486d574d09
newsam125 <newsam125@users.noreply.github.com>
2024-10-10 09:20:05 +0000

    deploy: 588955b80abf186dba41ff5dd3b370f3d8c5d8e7

---
+++ _next/static/WHYmqpf4Kul8Et8_j08jp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e2124b6f26d2ba3d.css","static/chunks/pages/index-dbfcfb503a1c9e7e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
